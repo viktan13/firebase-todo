290b541986d8a56a1e37dc758d502f08c6891c6d
Viktor <vik.tan@hotmail.com>
2022-11-23 15:02:40 -0600

    connected for Firebase database

---
+++ src/connectFirebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBnVkZCzhkiOJAePmx2cZ0GiHXWrgy4YKw",
+    authDomain: "todo-668a4.firebaseapp.com",
+    projectId: "todo-668a4",
+    storageBucket: "todo-668a4.appspot.com",
+    messagingSenderId: "1026052870705",
+    appId: "1:1026052870705:web:472da90567efb42f51fb10"
+};
+
+// Initialize Firebase
+const connectFirebase = initializeApp(firebaseConfig);
+
+export default connectFirebase;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import connectFirebase from './connectFirebase';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
