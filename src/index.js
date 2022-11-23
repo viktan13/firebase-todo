c573a8c651e32722fa9c582c381423c4e04eec41
Viktor <vik.tan@hotmail.com>
2022-11-23 15:12:42 -0600

    connected to database

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import connectFirebase from './connectFirebase';
+import connectFirebase from './connectDB';
+
 
 
 
