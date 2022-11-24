0bbb7389c41baeff770308fea95234c8530ddcaf
Viktor <vik.tan@hotmail.com>
2022-11-23 19:14:31 -0600

    another way to get the todoList from the Firebase database

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import './App.css';
 import db from './connectDB';
-import {collection, query, onSnapshot} from 'firebase/firestore';
+import {collection, query, onSnapshot, addDoc, Timestamp} from 'firebase/firestore';
 import {useEffect, useState} from "react";
+import CreateTaskForm from "./CreateTaskForm";
 
 function App() {
 
-  const[todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState([]);
 
-  useEffect(() => {
-  const todoListColRef = query(collection(db, 'todoList'));
-  onSnapshot(todoListColRef, (snapshot) => {
-      setTodoList(snapshot.docs.map(el => ({
-          id: el.id,
-          ...el.data(),
-      })))
-  })
-  });
-
+    useEffect(() => {
+        const todoListColRef = query(collection(db, 'todoList'));
+        onSnapshot(todoListColRef, (snapshot) => {
+            setTodoList(snapshot.docs.map(el => ({
+                ...el.data(),
+            })))
+        })
+    });
 
 
-  return (
-    <div>
+    return (
+        <div>
+            <CreateTaskForm />
+            <ul>
+                {todoList.map(el => (
+                    <li key={el.id}>{el.title}</li>
+                ))}
+            </ul>
+            <button onClick={addTo}>Add</button>
 
-      <ul>
-        {todoList.map(el => (
-            <li key={el.id}>{el.title}</li>
-        ))}
-      </ul>
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
---
+++ src/CreateTaskForm.js
@@ -0,0 +1,30 @@
+import db from './connectDB';
+import {collection, addDoc, Timestamp} from 'firebase/firestore';
+import {useState} from "react";
+
+
+const CreateTaskForm = () => {
+
+
+    const [title, setTitle] = useState('');
+
+    const addToList = (e) => {
+        e.preventDefault();
+        console.log(db);
+        addDoc(collection(db, 'todoList'), {
+            title,
+            done: false,
+
+        }).then(res => console.log(res)).catch(err => console.log(err));
+        setTitle('');
+    }
+
+    return (
+        <form>
+            <input type="text" placeholder="Enter task title" value={title} onChange={e => setTitle(e.target.value)}/>
+            <button type="submit" onClick={addToList}>Add to List</button>
+        </form>
+    );
+};
+
+export default CreateTaskForm;
